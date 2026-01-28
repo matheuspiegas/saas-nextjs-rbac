fd2310d7fb5342f915fd762746fc66478aee0cd7
matheuspiegas <matheusaugustopiegassales@gmail.com>
2026-01-28 13:50:08 -0300

    feat: registrando novas rotas

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -17,6 +17,10 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { createInvite } from './routes/invites/create-invite'
+import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
+import { updateMember } from './routes/members/update-members'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
@@ -89,6 +93,14 @@ app.register(deleteProject)
 app.register(getProject)
 app.register(getProjects)
 app.register(updateProject)
+
+// Member routes
+app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
+
+// Invite routes
+app.register(createInvite)
 
 app.listen({ port: 3333 }, (err, address) => {
   if (err) {
