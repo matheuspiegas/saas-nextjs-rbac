fc31d07aabaa5d45a829e4ea69f97eb83593b295
matheuspiegas <matheusaugustopiegassales@gmail.com>
2026-01-28 13:51:48 -0300

    feat: implementando rota para atualizar um membro

---
+++ apps/api/src/http/routes/members/update-members.ts
@@ -0,0 +1,59 @@
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function updateMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .put(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['members'],
+          summary: 'Update a member',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.uuid(),
+          }),
+          body: z.object({
+            role: roleSchema,
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (req, reply) => {
+        const userId = await req.getCurrentUserId()
+        const { slug, memberId } = req.params
+        const { membership, organization } = await req.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', 'User')) {
+          throw new UnauthorizedError("You're not allowed to update this member.")
+        }
+
+        const { role } = req.body
+
+        await prisma.member.update({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+          data: {
+            role,
+          },
+        })
+
+        reply.status(204).send()
+      }
+    )
+}
